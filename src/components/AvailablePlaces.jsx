aebd71748c2272677d6c700a6e79df4bdf545b9d
lviolari <lviolari2@gmail.com>
2025-04-24 21:13:37 +0300

    Section 136

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [AvailablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);    
+    }
+
+    fetchPlaces();
+  },[]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={AvailablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
